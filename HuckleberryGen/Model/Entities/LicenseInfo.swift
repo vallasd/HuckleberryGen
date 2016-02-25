255e50d405093f292b976a6e03bf8a9e9e086c7a
David Vallas <david_vallas@yahoo.com>
2016-02-24 21:55:43 -0500

    Added the GNU license to LicenseInfo

--- HuckleberryGen/Model/Entities/LicenseInfo.swift
+++ HuckleberryGen/Model/Entities/LicenseInfo.swift
@@ -23,19 +23,18 @@
 import Foundation
 
 struct LicenseInfo {
+    
     let name: String
     let company: String
     let contact1: String
     let contact2: String
     let type: LicenseType
     
-    
     func string(project: String, fileName: String) -> String {
         let companyInfo = company.isEmpty ? name : company
         let contact1Info = contact1.isEmpty ? contact1 : "(\(contact1))"
         let contact2Info = contact2.isEmpty ? contact2 : "(\(contact2))"
-        return "//   \(project)\n//   \(fileName) created (on: \(hgGenCurrentDate) for: \(name))\n//\n//   Copyright (c) \(hgGenCurrentYear) \(companyInfo) \(contact1Info) \(contact2Info)\n//\n\(type.staticText)\n\nimport Foundation\n"
-//        return "//   \(project)\n//   \(fileName) created by HuckleberryGen (on: \(hgGenCurrentDate) for: \(name))\n//\n//   Copyright (c) \(hgGenCurrentYear) \(companyInfo) \(contact1Info) \(contact2Info)\n//\n\(type.staticText)\n\nimport Foundation\n"
+        return "//   \(project)\n//   \(fileName) created by HuckleberryGen (on: \(hgGenCurrentDate) for: \(name))\n//\n//   Copyright (c) \(hgGenCurrentYear) \(companyInfo) \(contact1Info) \(contact2Info)\n//\n\(type.staticText)\n\nimport Foundation\n"
     }
     
     var needsMoreInformation: Bool {
@@ -77,11 +76,13 @@ extension LicenseInfo: HGEncodable {
 enum LicenseType: Int16 {
     case MIT = 0
     case AllRightsReserved = 1
+    case GNU = 2
     
     static func create(int int: Int) -> LicenseType {
         switch (int) {
         case 0: return .MIT
         case 1: return .AllRightsReserved
+        case 2: return .GNU
         default:
             HGReportHandler.shared.report("int: |\(int)| is not an LicenseType mapable, returning .MIT", type: .Error)
             return .MIT
@@ -96,6 +97,7 @@ enum LicenseType: Int16 {
         switch(self) {
         case .MIT: return "//   The MIT License (MIT)\n//\n//   Permission is hereby granted, free of charge, to any person obtaining a copy\n//   of this software and associated documentation files (the \"Software\"), to deal\n//   in the Software without restriction, including without limitation the rights\n//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n//   copies of the Software, and to permit persons to whom the Software is\n//   furnished to do so, subject to the following conditions:\n//\n//   The above copyright notice and this permission notice shall be included in all\n//   copies or substantial portions of the Software.\n//\n//   THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n//   SOFTWARE"
         case .AllRightsReserved: return "//   All Rights Reserved."
+        case .GNU: return "//  This project is free software: you can redistribute it and/or modify\n//  it under the terms of the GNU General Public License as published by\n//  the Free Software Foundation, either version 3 of the License, or\n//  (at your option) any later version.\n//\n//  This project is distributed in the hope that it will be useful,\n//  but WITHOUT ANY WARRANTY; without even the implied warranty of\n//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n//  GNU General Public License for more details.\n//\n//  You should have received a copy of the GNU General Public License\n//  along with this project.  If not, see <http://www.gnu.org/licenses/>."
         }
     }
 }
